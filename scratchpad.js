548a1c268213db78524fea136fdf4a84b5e7f2a5
vecchiotom <vecchiotom1@gmail.com>
2016-06-04 02:41:49 +0200

    Update scratchpad.js

--- scratchpad.js
+++ scratchpad.js
@@ -14,6 +14,7 @@
         
         $('viewport').style.background = "url(https://images7.alphacoders.com/351/351342.jpg) top left no-repeat";
 				$('hplogo').style.title = 'now Google is more beautiful!!!';
+				//this script is not completed! this is only the 0.0.1 alpha build! more version to be expected in next months!!!
 		    
    
 
