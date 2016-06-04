cc0573284f7409d05325764b2a5cafb9795bcc85
vecchiotom <vecchiotom1@gmail.com>
2016-06-04 00:39:27 +0000

    Head Script!
    This is the script that make all working! install it on your browser using tampermonkey, greasemonkey or others!

---
+++ scratchpad.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           GoogleBeautifulAdder
+// @namespace      Make Google more beautiful
+// @description    Add new styles and remove many unuseful stuff from google.
+// @include        *
+// @author         Tommaso Celano
+// ==/UserScript==
+
+	function $(id) {
+		return document.getElementById(id);
+	}
+
+	function clearInterface(){
+        
+        $('viewport').style.background = "url(https://images7.alphacoders.com/351/351342.jpg) top left no-repeat";
+				$('hplogo').style.title = 'now Google is more beautiful!!!';
+		    
+   
+
+
+
+
+	
+	}
+
+
+	clearInterface();
+
