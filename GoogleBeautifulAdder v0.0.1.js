887b2ddee65b5840e824ac257c51dbfe1e714c48
vecchiotom <vecchiotom1@gmail.com>
2016-06-21 00:05:55 +0200

    New Release!

--- GoogleBeautifulAdder v0.0.1.js
+++ GoogleBeautifulAdder v0.0.1.js
@@ -1,9 +1,9 @@
-// ==UserScript==
+
 // ==UserScript==
 // @name           GoogleBeautifulAdder
 // @namespace      Make Google more beautiful
 // @description    Add new styles and remove many unuseful stuff from google.
-// @include        https://www.google.*
+// @include        *
 // @author         Tommaso Celano
 // @version        0.0.2-alphabuild
 // ==/UserScript==
@@ -15,16 +15,14 @@
 	function $(id) {
 		return document.getElementById(id);
 	}
-    function gifsearch(){
-    $('lst-ib').value = 'filetype:gif WRITEHEREYOURSEARCH';
-    }
 
-	  function clearInterface(){
+	function clearInterface(){
         
         $('viewport').style.background = "url(https://images7.alphacoders.com/351/351342.jpg) top left no-repeat";
 				$('hplogo').title = 'Now Google is more beautiful';
+		    $('lst-ib').value = "Make a beautiful search!!!";
 		    $('fbar').style.margin = '550px auto';
-		    //initially i wanted to add the image search for extentions but it were very bugged so i'm working on it... in this release i've added some beautiful stuffs and an easter egg! find it!
+		    $('fbar').innerHTML = '<input type="submit" value="Search .gif images" style=" margin: 0 auto;"><input type="submit" value="Search .jpg images" style=" margin: 0 auto;"><input type="submit" value="Search .jpg images" style=" margin: 0 auto;"><input type="submit" value="Search .jpg images" style=" margin: 0 auto;"><input type="submit" value="Search .jpg images" style=" margin: 0 auto;">'
 		    
    
 
@@ -36,6 +34,5 @@
 
 
 	clearInterface();
-
-	clearInterface();
+		    //initially i wanted to add the image search for extentions but it were very bugged so i'm working on it... in this release i've added some beautiful stuffs and an easter egg! find it!
 
